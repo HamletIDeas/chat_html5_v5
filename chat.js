09f79c1d356196c052351001b3ad6b0562f05e05
hamlet666 <hamlet666@live.com.mx>
2014-02-15 19:14:30 -0600

    hola

--- chat.js
+++ chat.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var dust = require("dustjs-linkedin");
 var cons = require("consolidate");
+var http = require("http");
+var socketio = require("socket.io");
+
+
 
 var app = express();
-app.listen(8021);
+var servidor = http.createServer(app);
+servidor.listen(8021);
 
 //---- configuracion de carpetas estaticas ----
 app.use("/css", express.static(__dirname + "/css"));
@@ -15,3 +20,18 @@ app.set("views", __dirname + "/vistas");
 app.set("view engine", "dust");
 
 console.log("servidor web listo");
+
+app.get("/",function(req,res){
+	res.render("cliente");
+});
+//habilitamos el web socket
+var io = socketio.listen(servidor);
+
+
+io.sockets.on("connection", function(socket){
+	socket.on("mensaje_al_servidor", function(datosCliente){
+		
+		io.sockets.emit("mensaje_al_cliente",datosCliente);
+	});
+	
+});
